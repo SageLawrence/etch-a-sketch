221c85393dbfffd585cbdff64d7bc3db90a6717c
Sage Lawrence <sageblawrence@gmail.com>
2022-11-02 13:34:36 -0700

    added some comments to start incrementing box color by 10% but have not finished implementation

--- js.js
+++ js.js
@@ -3,14 +3,19 @@ function boxCreate(edgeCount) { // function to create gid of specified size
     for (let i = 0; i < edgeCount; i++) { // loops through for column creation
 
         let divBox = document.createElement('div'); // creates one div in memory that will make a column
+
         divBox.classList.add('divBox'); // adds a CSS class for column traits
+        
         let container = document.getElementById('container'); // selects the main container div from the HTML
+
         container.appendChild(divBox); // adds the column within the container div
 
         for (let j = 0; j < edgeCount; j++) { // loops through for div box creation within columns
 
             let div = document.createElement('div'); // creates one div in memory that will make a box
+
             div.classList.add('div'); // adds a CSS class for box traits
+
             divBox.appendChild(div); // adds the box within the column
 
         }
@@ -57,10 +62,25 @@ function hoverAdd() {
 
     divs.forEach(div => div.addEventListener('mouseover', sketch)); // adds listener for mouseover to call sketch function
 
+    divs.forEach(div => div.style.backgroundColor = 'rgb(255, 255,255'); // adds white in rgb as the original background color
+
 }
 
-function sketch() { // alters box after it is hovered over
+function sketch() { // alters box when it is hovered over
 
-    this.classList.add('hovered'); // adds class to color the box in
+    // console.log(this.style.backgroundColor);
+    //let backgroundColor = this.style.backgroundColor; // retrieves current background color
+
+    //if (backgroundRed > 0) { // not allowing for rgb values to go negative
+
+        //backgroundRed -= 25.5;
+        //backgroundGreen -= 25.5;
+        //backgroundBlue -= 25.5;
+
+        //backgroundColor = 'rgb(' + backgroundRed + ',' + backgroundGreen + ',' + backgroundBlue + ')'; // sets background color of div by component
+
+        this.style.backgroundColor = 'black'; // sets background color of div based on mouseovers
+
+    ///}
 
 }
