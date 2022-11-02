4e5fbdbf39c521fb978f714d0e0408a9b8324ec9
Sage Lawrence <sageblawrence@gmail.com>
2022-11-01 21:26:28 -0700

    added mouseover function to set divs to black

--- js.js
+++ js.js
@@ -1,8 +1,24 @@
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16; i++) { // loops through for column creation
 
-    let divBox = document.createElement('div'); // creates one div to make a box
-    divBox.classList.add('divBox'); // adds a CSS class for box traits
-    let container = document.getElementById('container'); // selects the container div from the HTML
-    container.appendChild(divBox); // adds the box within the container div
+    let divBox = document.createElement('div'); // creates one div in memory that will make a column
+    divBox.classList.add('divBox'); // adds a CSS class for column traits
+    let container = document.getElementById('container'); // selects the main container div from the HTML
+    container.appendChild(divBox); // adds the column within the container div
 
+    for (let j = 0; j <  16; j++) { // loops through for div box creation within columns
+
+        let div = document.createElement('div'); // creates one div in memory that will make a box
+        div.classList.add('div'); // adds a CSS class for box traits
+        divBox.appendChild(div); // adds the box within the column
+
+    }
+
+}
+
+const divs = document.querySelectorAll('.div');
+
+divs.forEach(div => div.addEventListener('mouseover', sketch));
+
+function sketch() {
+    this.classList.add('hovered');
 }
