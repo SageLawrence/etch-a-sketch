7a64a79b0d5bbd6c284072b43269bf7bf0bfcd46
Sage Lawrence <sageblawrence@gmail.com>
2022-11-02 11:20:41 -0700

    completed code to make grid size selectable

--- js.js
+++ js.js
@@ -1,33 +1,56 @@
-for (let i = 0; i < 100; i++) { // loops through for column creation
+function boxCreate(edgeCount) { // function to create gid of specified size
 
-    let divBox = document.createElement('div'); // creates one div in memory that will make a column
-    divBox.classList.add('divBox'); // adds a CSS class for column traits
-    let container = document.getElementById('container'); // selects the main container div from the HTML
-    container.appendChild(divBox); // adds the column within the container div
+    for (let i = 0; i < edgeCount; i++) { // loops through for column creation
 
-    for (let j = 0; j <  100; j++) { // loops through for div box creation within columns
+        let divBox = document.createElement('div'); // creates one div in memory that will make a column
+        divBox.classList.add('divBox'); // adds a CSS class for column traits
+        let container = document.getElementById('container'); // selects the main container div from the HTML
+        container.appendChild(divBox); // adds the column within the container div
 
-        let div = document.createElement('div'); // creates one div in memory that will make a box
-        div.classList.add('div'); // adds a CSS class for box traits
-        divBox.appendChild(div); // adds the box within the column
+        for (let j = 0; j <  edgeCount; j++) { // loops through for div box creation within columns
+
+            let div = document.createElement('div'); // creates one div in memory that will make a box
+            div.classList.add('div'); // adds a CSS class for box traits
+            divBox.appendChild(div); // adds the box within the column
+
+        }
+
+    }
+
+    hoverAdd()
+
+}
+
+
+const button = document.querySelector('.selector'); //
+
+button.addEventListener('click', sizeSelect);
+
+function sizeSelect() {
+
+    let edgeCount = prompt('What size grid would you like? (Max 100)');
+
+    if (edgeCount <= 100) {
+
+        boxCreate(edgeCount);
+
+    } else {
+        
+        alert('Box count is too high!');
 
     }
 
 }
 
-const divs = document.querySelectorAll('.div'); // selects all divs with class .div
+function hoverAdd() {
 
-divs.forEach(div => div.addEventListener('mouseover', sketch)); // adds listener for mouseover to call sketch function
+    const divs = document.querySelectorAll('.div'); // selects all divs with class .div
+    divs.forEach(div => div.addEventListener('mouseover', sketch)); // adds listener for mouseover to call sketch function
 
-function sketch() { // 
-    this.classList.add('hovered');
 }
 
-const button = document.querySelector('button');
+function sketch() { // alters box after it is hovered over
 
-button.addEventListener('click', sizeSelect);
+    this.classList.add('hovered'); // adds class to color the box in
 
-function sizeSelect() {
-    let input = prompt('What size grid would you like? (Max 100)');
-    console.log(input);
 }
